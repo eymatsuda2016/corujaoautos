261c3f8ec7495f03bcce06a0f923c7e91d9d1653
Eduardo Y matsuda <eymatsuda2016@outlook.com>
2023-12-02 16:10:03 -0300

    Testado a classe dominio Categoria

--- src/main/java/com/eduardomatsuda/corujaoautos/resources/CategoriaResource.java
+++ src/main/java/com/eduardomatsuda/corujaoautos/resources/CategoriaResource.java
@@ -1,15 +1,26 @@
 package com.eduardomatsuda.corujaoautos.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.eduardomatsuda.corujaoautos.domain.Categoria;
+
 @RestController
 @RequestMapping(value= "/categorias")
 public class CategoriaResource {
-	
-	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return  "Rest Funcionando";
+
+		@RequestMapping(method = RequestMethod.GET)
+		public List<Categoria> listar(){
+			Categoria cat1= new Categoria(1, "Carros");
+			Categoria cat2 = new Categoria(2, "Caminhões e Onibus");
+			
+			List<Categoria> lista = new ArrayList<>();
+			lista.add(cat1);
+			lista.add(cat2);
+			return lista;
+		}	
 	}
-}
