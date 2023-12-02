59de373d883bb00af60c58a3b91e2d554feea8e3
Eduardo Y matsuda <eymatsuda2016@outlook.com>
2023-12-02 15:07:52 -0300

    Projeto AEC Inicial

---
+++ src/main/java/com/eduardomatsuda/corujaoautos/CorujaoautosApplication.java
@@ -0,0 +1,13 @@
+package com.eduardomatsuda.corujaoautos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorujaoautosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorujaoautosApplication.class, args);
+	}
+
+}
