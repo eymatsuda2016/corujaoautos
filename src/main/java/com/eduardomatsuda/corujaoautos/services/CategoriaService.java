a59d4d2e69316f55ba82e3a42e26f94b802ade38
Eduardo Y matsuda <eymatsuda2016@outlook.com>
2023-12-09 17:46:36 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/eduardomatsuda/corujaoautos/services/CategoriaService.java
+++ src/main/java/com/eduardomatsuda/corujaoautos/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.eduardomatsuda.corujaoautos.domain.Categoria;
 import com.eduardomatsuda.corujaoautos.repositories.CategoriaRepository;
+import com.eduardomatsuda.corujaoautos.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,6 +17,8 @@ public class CategoriaService {
 	
 	public Categoria find (Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 }
+
