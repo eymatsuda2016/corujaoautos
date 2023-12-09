a0260271536e0ac3f708dd0731c245dcea2d08b4
Eduardo Y matsuda <eymatsuda2016@outlook.com>
2023-12-09 11:21:26 -0300

    Produto e Associação Muitos para Muitos

--- src/main/java/com/eduardomatsuda/corujaoautos/CorujaoautosApplication.java
+++ src/main/java/com/eduardomatsuda/corujaoautos/CorujaoautosApplication.java
@@ -8,12 +8,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.eduardomatsuda.corujaoautos.domain.Categoria;
+import com.eduardomatsuda.corujaoautos.domain.Produto;
 import com.eduardomatsuda.corujaoautos.repositories.CategoriaRepository;
+import com.eduardomatsuda.corujaoautos.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CorujaoautosApplication implements CommandLineRunner {    
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	@Autowired
+	private ProdutoRepository produtoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CorujaoautosApplication.class, args);
@@ -24,6 +28,18 @@ public class CorujaoautosApplication implements CommandLineRunner {
 		Categoria cat1 = new Categoria (null, "Autos e Carros");
 		Categoria  cat2 = new Categoria (null, "Caminhões e Ônibus");
 		
+		Produto p1 = new Produto(null, "Jogo de Velas VW SpaceFox", 120.00);
+		Produto p2 = new Produto(null, "Jogo de Lampadas do Painel", 130.00);
+		Produto p3 = new Produto(null, "Jogo de Cabos de Velas VW SpaceFox", 125.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));	
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 	}
 }
--- src/main/java/com/eduardomatsuda/corujaoautos/domain/Categoria.java
+++ src/main/java/com/eduardomatsuda/corujaoautos/domain/Categoria.java
@@ -1,12 +1,15 @@
 package com.eduardomatsuda.corujaoautos.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 
 @Entity
 public class Categoria implements Serializable {
@@ -16,6 +19,9 @@ public class Categoria implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
+	
+	@ManyToMany(mappedBy="categorias")
+	private List<Produto> produtos=new ArrayList<>();
 	
 	public Categoria() {
 	}
@@ -41,6 +47,14 @@ public class Categoria implements Serializable {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+	
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
+	}
 
 	@Override
 	public int hashCode() {
@@ -58,4 +72,6 @@ public class Categoria implements Serializable {
 		Categoria other = (Categoria) obj;
 		return Objects.equals(id, other.id);
 	}
+
+
 }
