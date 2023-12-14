3d9dec06663d1f9d945217464a00e0bae7e171d2
Eduardo Y matsuda <eymatsuda2016@outlook.com>
2023-12-13 22:37:43 -0300

    Endpoint /clientes{id} disonivel

---
+++ src/main/java/com/eduardomatsuda/corujaoautos/services/ClienteService.java
@@ -0,0 +1,24 @@
+package com.eduardomatsuda.corujaoautos.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.eduardomatsuda.corujaoautos.domain.Cliente;
+import com.eduardomatsuda.corujaoautos.repositories.ClienteRepository;
+import com.eduardomatsuda.corujaoautos.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente find (Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+}
+
